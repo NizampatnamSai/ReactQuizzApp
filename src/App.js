523f21d5504e9b6b6dbc2c3e976bcaf6b58e4779
Nizampatnamsai <jesusjoshuajesus@gmail.com>
2022-06-06 01:09:48 +0530

    technical done!!

--- src/App.js
+++ src/App.js
@@ -45,11 +45,16 @@ function App() {
   const [showScore, setShowScore] = useState(false);
   const [score, setScore] = useState(0);
   const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [wrongscore,setWrongscore]=useState(0);
 
   function handleAnswer(isCorrect) {
     if (isCorrect) {
       setScore(score + 1);
     }
+    if(!isCorrect){
+      setWrongscore(wrongscore+1)
+    }
+    
 
     const nextQuestion = currentQuestion + 1;
     if (nextQuestion < questions.length) {
@@ -57,21 +62,35 @@ function App() {
     } else {
       setShowScore(true);
     }
+  } 
+
+
+  let actualScore=(score*5-(wrongscore*4));
+
+  const styles={
+    background:'red',
+    // progress::-webkit-progress-value {background-color: green !important;}
   }
 
   return (
     <div className="app">
       {showScore ? (
         <div className="score-section">
-          You scored {score} out of {questions.length}
+          <b className="points">Points</b> <span>{actualScore}</span>
+          <div className="result_res"><b>Right Ans</b>  <progress className="rightans" value={score} max={4} />
+          <b>Wrong Ans</b> <progress className="wrongans" value={wrongscore} max={4} />
+          </div> 
+         
+        <div className="result_footer">  You scored {score} out of {questions.length} </div>
         </div>
       ) : (
         <>
           <div className="question-section">
             <div className="question-count"> 
-            <h4>Points</h4> <small>{score}</small>
-             <div>Right Ans <progress className="rightans" value={score} max={4} />Wrong Ans <progress className="rightans" value={score} max={4} /></div> 
-              <span>Question {currentQuestion + 1}</span>/{questions.length}
+            <b className="points">Points</b> <span className="score">{actualScore}</span>
+             <div className="progress"><b>Right Ans</b> <progress className="rightans" value={score} max={4} /></div> 
+            <div> <b>Wrong Ans</b> <progress className="wrongans" value={wrongscore} max={4} style={styles}></progress></div>
+              <span className="question">Question {currentQuestion + 1}</span>/{questions.length}
             
             </div>
             <div className="question-text">
@@ -91,6 +110,7 @@ function App() {
               )
             )}
           </div>
+          <b>Note: Right ans is 5 points and Wrong ans leads to -4 ponits</b>
          
         </>
          
