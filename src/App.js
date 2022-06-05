a59e5aafcc83968ddfcd0afc168999242807564f
Nizampatnamsai <jesusjoshuajesus@gmail.com>
2022-06-05 19:09:02 +0530

    Question part

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+const questions = [
+  {
+    questionText: "What language is spoken in Brasil?",
+    answerOptions: [
+      { answerText: "Portugues", isCorrect: true },
+      { answerText: "English", isCorrect: false },
+      { answerText: "French", isCorrect: false },
+      { answerText: "German", isCorrect: false },
+    ],
+  },
+  {
+    questionText:
+      "Which countries have the hieghest and lowest life expectancy in the world?",
+    answerOptions: [
+      { answerText: "Australia and Afghnistan", isCorrect: false },
+      { answerText: "Japan and sierra", isCorrect: true },
+      { answerText: "Italy", isCorrect: false },
+      { answerText: "Brasil", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "Which company created the iPhone?",
+    answerOptions: [
+      { answerText: "Intel", isCorrect: false },
+      { answerText: "Amazon", isCorrect: false },
+      { answerText: "Apple", isCorrect: true },
+      { answerText: "Microsoft", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "How to learn to program?",
+    answerOptions: [
+      { answerText: "Practising what you learn", isCorrect: true },
+      { answerText: "Watching video", isCorrect: false },
+      { answerText: "Reading", isCorrect: false },
+      { answerText: "Writing", isCorrect: false },
+    ],
+  },
+];
 
 function App() {
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+
+  function handleAnswer(isCorrect) {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {showScore ? (
+        <div className="score-section">
+          Você pontuou {score} de {questions.length}
+        </div>
+      ) : (
+        <>
+          <div className="question-section">
+            <div className="question-count">
+              <span>Questão {currentQuestion + 1}</span>/{questions.length}
+            </div>
+            <div className="question-text">
+              {questions[currentQuestion].questionText}
+            </div>
+          </div>
+
+          <div className="answer-section">
+            {questions[currentQuestion].answerOptions.map(
+              (answerOption, index) => (
+                <button
+                  onClick={() => handleAnswer(answerOption.isCorrect)}
+                  key={index}
+                >
+                  {answerOption.answerText}
+                </button>
+              )
+            )}
+          </div>
+        </>
+      )}
     </div>
   );
 }
